3a6972cce5f3a25b642bbfb170f0de61d72d3604
Updoong <lightrainme@gmail.com>
2025-10-18 11:45:24 +0900

    대가성문구 업데이트

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
       <div className="max-w-5xl mx-auto px-4 text-center">
         <p>© 2025 Refilter. All rights reserved.</p>
         <p className="mt-1 text-xs text-gray-500">
-          본 서비스는 쿠팡 파트너스 활동의 일환으로, 일정액의 수수료를 제공받을 수 있습니다.
+          이 포스팅은 쿠팡 파트너스 활동의 일환으로, 이에 따른 일정액의 수수료를 제공받습니다.
         </p>
       </div>
     </footer>
