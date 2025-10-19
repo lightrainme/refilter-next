8043c2105b31e730b776cb1e3ee2cdac7c6e6dc4
Updoong <lightrainme@gmail.com>
2025-10-19 16:00:03 +0900

    서치화면 스타일 수정

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -18,12 +18,14 @@ export default function SearchPage() {
     <div className="min-h-screen bg-gray-100">
       <div className="max-w-xl mx-auto pt-28 px-6">
         {/* 로고: 비율 유지 + 반응형 */}
-        <div className="flex-column justify-items-center">
-          <div className="relative w-30 h-30 rounded-full aspect-[1/1]">
-            <Image src="/human-ai-shadow.png" alt="캐릭터" fill className="absolute bottom-0 left-1/2 -translate-x-1  object-cover scale-100" />
+        <div className="relative flex-column justify-center justify-items-center w-full">
+          <div className="relative block w-full h-30">
+              <Image src="/human-ai-shadow.png" alt="캐릭터" fill style={{ objectFit:'contain', objectPosition:'center', }}className="realtive" />
           </div>
-          <div className="relative w-50 md:w-60 lg:w-70 h-20 mx-auto">
-            <Image src="/logo.svg" alt="로고" fill className="object-fit" priority />
+          <div className="relative w-full h-15 mt-1 mb-1">
+              <Image src="/logo.svg" alt="로고" fill
+              style={{ objectFit:'contain', objectPosition:'center', }} 
+              className="object-fit object-center" priority />
           </div>
           <p className="text-center text-gray-800 mb-5">신뢰성 있는 리뷰를 필터링 해드립니다</p>
         </div>
@@ -34,11 +36,11 @@ export default function SearchPage() {
             value={keyword}
             onChange={(e) => setKeyword(e.target.value)}
             placeholder="검색어를 입력해주세요 ex.아이폰"
-            className="flex-1 border border-gray-200 px-4 py-3 rounded-md bg-white placeholder-gray-400"
+            className="flex-1 border box-border border-gray-200 px-4 py-3 rounded-md bg-white placeholder-gray-400"
           />
           <button
             type="submit"
-            className="cursor-pointer px-6 py-2 rounded-md bg-purple-900 shadow hover:bg-indigo-900 text-white border-purple-950"
+            className="cursor-pointer box-border px-6 py-2 rounded-md bg-purple-900 shadow hover:bg-indigo-900 text-white border-purple-950"
           >
             검색
           </button>
